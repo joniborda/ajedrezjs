c00555c148f1b2be3e32626297f0f6eed687aaf2
joni <joniborda>
2018-02-14 12:49:41 -0300

    cuando haces click muestra los posibles movimientos

--- main.js
+++ main.js
@@ -46,9 +46,25 @@ for (var j = 0; j < 8; j++) {
 for (var j = 0; j < 8; j++) {
 	for (var i = 0; i < 8; i++) {
 		if (tablero.fichas[j][i]) {
+			$(tablero.fichas[j][i].ficha).click(function(e) {
+	  			tablero.tablero.find('.casilla_habilitada').remove();
+				if (tablero.fichas[parseInt($(this).attr('x'))][parseInt($(this).attr('y'))].color == tablero.jugadores[tablero.turno].color) {
+					for (var o = 0; o < 8; o++) {
+						for (var p = 0; p < 8; p++) {
+							if (tablero.fichas[parseInt($(this).attr('x'))][parseInt($(this).attr('y'))].habilitadaMover(o, p)) {
+								if (tablero.casillas[o][p].find('.casilla_habilitada').length === 0) {
+									tablero.casillas[o][p].append('<div class="casilla_habilitada"><div>');
+								}
+							}
+						}
+					}
+				}
+			});
+
 			$(tablero.fichas[j][i].ficha).draggable({
 				containment: tablero.tablero,
 				start: function(event, ui) {
+					tablero.tablero.find('.casilla_habilitada').remove();
 					if (tablero.fichas[parseInt($(this).attr('x'))][parseInt($(this).attr('y'))].color == tablero.jugadores[tablero.turno].color) {
 						for (var o = 0; o < 8; o++) {
 							for (var p = 0; p < 8; p++) {
