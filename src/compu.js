207a31f846948afd724c4cf9587b684e7695512b
joniborda <jonathanmatiasborda@gmail.com>
2018-03-18 21:28:37 -0300

    empiezo a ver como deberia mover la compu

---
+++ src/compu.js
@@ -0,0 +1,24 @@
+/** piezas mias que estan atacadas **/
+function piezas_atacadas(jugador) {
+	var color = jugador.getColor();
+}
+
+/** piezas mias que estan defendidas **/
+function piezas_defendidas() {
+
+}
+
+/** piezas que atacan a alguna pieza enemiga **/
+function piezas_que_ataca() {
+
+}
+
+/** piezas que pueden atacar al rey enemigo pero tiene una pieza en el medio **/
+function piezas_atacan_rey() {
+
+}
+
+/** piezas que no dejan que el rey enemigo muevan en alguna direccion **/
+function piezas_paran_rey() {
+
+}
